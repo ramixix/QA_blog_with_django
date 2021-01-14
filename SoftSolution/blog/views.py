c5d911c2c058e62b0c51cdedab318c7921da1619
ramin <xram1nx.h@gmail.com>
2021-01-14 19:08:19 +0000

    view.py dosyadaki home ve about viewları ve bu viewlar icin html templateleri oluşturuldu.

--- SoftSolution/blog/views.py
+++ SoftSolution/blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def Homeview(request):
+    return render(request, 'blog/home.html')
+
+def About(request):
+    return render(request, 'blog/about.html')
