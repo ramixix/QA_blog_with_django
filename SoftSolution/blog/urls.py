e2d252266e551dcf13868976ed5c593afaafd18c
ramin <xram1nx.h@gmail.com>
2021-01-14 20:39:31 +0000

    kullanıcılarımızın kendi postlarını güncelleyebilmesi için views.py'de post_update view'ı oluşturuldu

--- SoftSolution/blog/urls.py
+++ SoftSolution/blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import Homeview, About, PostCreateView, PostDetailView
+from .views import Homeview, About, PostCreateView, PostDetailView, PostUpdateView, PostDeleteView
 
 
 urlpatterns = [
@@ -7,5 +7,6 @@ urlpatterns = [
     path('about/', About, name="blog-about"),
     path('post/create/', PostCreateView.as_view(), name="post-create"),
     path('post/<int:pk>/', PostDetailView.as_view(), name="post-detail"),
-
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name="post-update"),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name="post-delete"),
 ]
--- SoftSolution/blog/views.py
+++ SoftSolution/blog/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from .models import Post
-from django.views.generic import ListView, CreateView, DetailView
-from django.contrib.auth.mixins import LoginRequiredMixin
-
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy, reverse
 # Create your views here.
 
 class Homeview(ListView):
@@ -28,3 +28,30 @@ class PostCreateView(LoginRequiredMixin, CreateView):
 class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/detail_post.html'
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    template_name = 'blog/create_post.html'
+    fields = ['title', 'category', 'body', 'snippet']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author :
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'blog/delete_post.html'
+    success_url = reverse_lazy('blog-home')
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author :
+            return True
+        return False
