f08ce2fb10c818a3d177c20afef538cc7cd81040
Adem <ademcapan37@gmail.com>
2021-01-15 21:37:29 +0300

    kullanicilarimizin postlara yorum yapabilmesi icin models.py'de Comment modeli olusturuldu

--- SoftSolution/blog/models.py
+++ SoftSolution/blog/models.py
@@ -34,3 +34,24 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.TextField(max_length=5000)
+    like = models.ManyToManyField(User, blank=True, related_name="comment_like")
+    dislike = models.ManyToManyField(User, blank=True, related_name="comment_dislike")
+    comment_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{str(self.post.title)}-{str(self.user.username)}'
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={ 'pk' : self.pk })
+
+    def all_likes(self):
+        return self.like.count()
+
+    def all_dislikes(self):
+        return self.dislike.count()
