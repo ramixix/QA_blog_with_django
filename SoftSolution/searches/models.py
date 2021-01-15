3dc0414358da394465424b45f839c99566e27de2
fatih <fatih.ktu0@gmail.com>
2021-01-15 23:10:11 +0300

    search yapilabilmesi ve bu search'larin database'e kaydedilmesi icin models.py'da SearchQuery eklendi

--- SoftSolution/searches/models.py
+++ SoftSolution/searches/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+
+class SearchQuery(models.Model):
+
+	user = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True, on_delete= models.SET_NULL)
+	query = models.CharField(max_length=256)
+	timestamp = models.DateTimeField(auto_now_add=True)
+
+
+	def __str__(self):
+	        return '{}-{}'.format(self.query, str(self.timestamp))
