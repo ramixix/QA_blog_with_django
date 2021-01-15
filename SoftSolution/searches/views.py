b9107eb9fe6ffde9a35c2956cf1850aa9120694b
fatih <fatih.ktu0@gmail.com>
2021-01-15 23:38:17 +0300

    kullanicilarin yaptigi sorgularin view.html'de listelenmesi icin search_view olusturuldu

--- SoftSolution/searches/views.py
+++ SoftSolution/searches/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .models import SearchQuery
+from blog.models import Post, BlogPostManager
+
 
 # Create your views here.
+
+
+def search_view(request):
+	query = request.GET.get('q', None)
+	user = None
+	if request.user.is_authenticated:
+		user = request.user
+
+	context = {"query":query}
+	if query is not None:
+		SearchQuery.objects.create(user=user, query=query)
+		blog_list = Post.objects.search(query=query)
+		context['blog_list'] = blog_list
+
+	
+
+	return render(request, 'searches/view.html', context)
