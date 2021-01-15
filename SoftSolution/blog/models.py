a42b8e33fba4b605eb3b85fccfa39fb62c463cfb
fatih <fatih.ktu0@gmail.com>
2021-01-15 23:42:43 +0300

    gelen query sorgulamasi database'de aranmasi icin BlogPostManager ve BlogPostQuerySet class'lari eklendi

--- SoftSolution/blog/models.py
+++ SoftSolution/blog/models.py
@@ -2,7 +2,37 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 from django.urls import reverse
+from django.conf import settings
+from django.db.models import Q
 # Create your models here.
+
+
+
+
+class BlogPostQuerySet(models.QuerySet):
+    def search(self, query):
+        search_area = (
+                Q(title__icontains=query) |
+                Q(body__icontains=query) |
+                Q(category__icontains=query) |
+                Q(author__username__icontains=query) |
+                Q(author__first_name__icontains=query) |
+                Q(author__last_name__icontains=query) |
+                Q(snippet__icontains=query) |
+                Q(author__email__icontains=query)
+        )
+        return self.filter(search_area)
+
+class BlogPostManager(models.Manager):
+    def get_queryset(self):
+        return BlogPostQuerySet(self.model, using=self._db)
+
+    def search(self, query=None):
+        if query is None:
+            return self.get_queryset().none()
+        return self.get_queryset().search(query)
+
+
 
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -14,7 +44,7 @@ class Post(models.Model):
     body = models.TextField()
     like = models.ManyToManyField(User, blank=True, related_name="likes")
     dislike = models.ManyToManyField(User, blank=True, related_name="dislikes")
-    
+    objects = BlogPostManager()
 
     def __str__(self):
         return self.title
@@ -27,6 +57,10 @@ class Post(models.Model):
 
     def all_dislikes(self):
         return self.dislike.count()
+
+    @property
+    def number_of_comments(self):
+        return Comment.objects.filter(post=self).count()
 
 class Category(models.Model):
     name = models.CharField(max_length=150)
