208d26ece19a9374ed759ecf7c1bcf0d70bf518f
fatih <fatih.ktu0@gmail.com>
2021-01-15 23:40:07 +0300

    urls.py'de search_view url'i verildi

--- SoftSolution/SoftSolution/urls.py
+++ SoftSolution/SoftSolution/urls.py
@@ -18,6 +18,8 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_view
 from django.conf import settings
 from django.conf.urls.static import static
+from searches.views import search_view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,6 +27,7 @@ urlpatterns = [
     path('user/', include("user.urls")),
     path('login/', auth_view.LoginView.as_view(template_name="user/login.html"), name='login'),
     path('logout/', auth_view.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
+    path('search/', search_view)
 
 ]
 
