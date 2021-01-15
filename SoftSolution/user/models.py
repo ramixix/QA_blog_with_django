5cfe9c3beda69abf4df6daf35fafb3b7552e5308
fatih <fatih.ktu0@gmail.com>
2021-01-15 16:28:30 +0300

    kullanicilarin profil sayfasi icin models.py de profile modeli olusturuldu

--- SoftSolution/user/models.py
+++ SoftSolution/user/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True, null=True)
+    image = models.ImageField(default='default.jpg', upload_to="profile_img")
+    education = models.CharField(max_length=255, blank=True, null=True)
+    website_url = models.CharField(max_length=255, blank=True, null=True)
+    linkin_url = models.CharField(max_length=255, blank=True, null=True)
+    facebook_url = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.user.username}_profile'
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **  kwargs)
+
+        img = Image.open(self.image.path)
+        if img.width > 300 or img.height > 300:
+            final_size = (300, 300)
+            img.thumbnail(final_size)
+            img.save(self.image.path)
