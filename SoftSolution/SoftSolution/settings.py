384c4a7f125452ed562298507d50d466dc2c167f
Adem <ademcapan37@gmail.com>
2021-01-15 13:21:21 +0300

    sitemize resimlerin eklenebilmesi icin settings.py'de media root'u ve urlsi olusturuldu ve otomatik olarak media klasoru olustu

--- SoftSolution/SoftSolution/settings.py
+++ SoftSolution/SoftSolution/settings.py
@@ -65,6 +65,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'blog.custom_context_processor.subject_renderer',
+
             ],
         },
     },
@@ -123,3 +125,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = '/media/'
