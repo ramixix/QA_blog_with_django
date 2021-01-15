275bc1f649c5307835bae63ba07f11a61e35acf5
fatih <fatih.ktu0@gmail.com>
2021-01-15 16:36:28 +0300

    kullancilarin profilini guncellemesi icin profile ve user update form'lari olusturuldu

--- SoftSolution/user/forms.py
+++ SoftSolution/user/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 class SingupForm(UserCreationForm):
     first_name = forms.CharField(max_length=30)
@@ -11,3 +12,14 @@ class SingupForm(UserCreationForm):
         model = User
         fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2' ]
 
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email' ]
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['bio', 'education', 'website_url', 'linkin_url', 'facebook_url', 'image']
