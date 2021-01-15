9050a86a49d9c2267fae0e552843dce6710c9185
Adem <ademcapan37@gmail.com>
2021-01-15 21:59:32 +0300

    kullanicilara yorum yapma alani olusturmak icin forms.py'de Comment form olusturuldu

--- SoftSolution/blog/forms.py
+++ SoftSolution/blog/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Post, Category
+from .models import Post, Category, Comment
 
 choices = Category.objects.all().values_list('name', 'name')
 choices_list = []
@@ -14,3 +14,17 @@ class PostCreateForm(forms.ModelForm):
         widgets = {
             'category' : forms.Select(choices=choices_list)
         }
+
+
+class NewCommentForm(forms.ModelForm):
+    content = forms.CharField(label="", widget=forms.Textarea(
+        attrs={
+            'class': 'form-control',
+            'placeholder': 'Comment here !',
+            'rows': 4,
+            'cols': 50
+        }))
+
+    class Meta:
+        model = Comment
+        fields = ['content']
